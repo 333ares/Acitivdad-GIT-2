09b23ff716da844f5c8a4524320365ddc315d926
333ares <aresgp5335@gmail.com>
2024-12-16 19:44:38 +0100

    Tabulación
    He tabulado.

--- src/Actividad_2/MatrizApp.java
+++ src/Actividad_2/MatrizApp.java
@@ -6,12 +6,12 @@ public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO: Implementar las funciones según las ramas asignadas
-		
-		//Generar y mostrar la matriz de números aleatorios
+
+		// Generar y mostrar la matriz de números aleatorios
 		int[][] matriz = new int[5][5];
-		
+
 		Random r = new Random();
-		
+
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
 				matriz[i][j] = r.nextInt(101);
@@ -19,16 +19,15 @@ public class MatrizApp {
 		}
 
 		System.out.println("--- MATRIZ ---");
-		
-		for (int i = 0; i < matriz.length; i++) {		
+		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
-				System.out.print(matriz[i][j] + " "); 
+				System.out.print(matriz[i][j] + " ");
 			}
-			System.out.println();	
+			System.out.println();
 		}
-		
-		//Búsqueda de un número
-		
+
+		// Búsqueda de un número
+
 	}
 
 }
