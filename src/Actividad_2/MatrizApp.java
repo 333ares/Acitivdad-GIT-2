6f703ec43616dff8c655d373db3cf45c72a895b3
333ares <aresgp5335@gmail.com>
2024-12-16 19:39:35 +0100

    Matriz y random
    He creado la matriz y añadido el random para rellenar la matriz.

--- src/Actividad_2/MatrizApp.java
+++ src/Actividad_2/MatrizApp.java
@@ -1,11 +1,16 @@
 package Actividad_2;
 
+import java.util.Random;
+
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO: Implementar las funciones según las ramas asignadas
 		
 		//Generar y mostrar la matriz de números aleatorios
+		int[][] matriz = new int[5][5];
+		
+		Random r = new Random();
 		
 		//Búsqueda de un número
 		
