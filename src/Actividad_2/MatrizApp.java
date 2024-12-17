57d43fed9b75f1c41d13dbcea805d4e6db0d182b
333ares <aresgp5335@gmail.com>
2024-12-17 17:09:24 +0100

    Cambio de nombre de la matriz
    He cambiado el nombre de la matriz, de matriz a numeros.

--- src/Actividad_2/MatrizApp.java
+++ src/Actividad_2/MatrizApp.java
@@ -8,21 +8,21 @@ public class MatrizApp {
 		// TODO: Implementar las funciones según las ramas asignadas
 
 		// matriz-inicial
-		int[][] matriz = new int[5][5]; // Definir matriz.
+		int[][] numeros = new int[5][5]; // Definir matriz.
 
 		Random r = new Random(); // Añado el random que usaré para rellenar la matriz.
 
-		for (int i = 0; i < matriz.length; i++) { // Este bucle se encarga de las filas.
-			for (int j = 0; j < matriz.length; j++) { // Este bucle se encraga de las columnas.
-				matriz[i][j] = r.nextInt(101); // Relleno la matriz con el random y le establezco un rango de números
+		for (int i = 0; i < numeros.length; i++) { // Este bucle se encarga de las filas.
+			for (int j = 0; j < numeros.length; j++) { // Este bucle se encraga de las columnas.
+				numeros[i][j] = r.nextInt(101); // Relleno la matriz con el random y le establezco un rango de números
 												// que puede generar (1 - 100).
 			}
 		}
 
 		System.out.println("--- MATRIZ ---"); //Muestro un título para la matriz.
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				System.out.print(matriz[i][j] + " "); //Muestro la matriz con espacios.
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; j++) {
+				System.out.print(numeros[i][j] + " "); //Muestro la matriz con espacios.
 			}
 			System.out.println(); //Hago que se muestre línea por línea.
 		}
