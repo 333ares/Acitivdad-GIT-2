b26851eddd222784c09d4533026845a4d31515e1
333ares <aresgp5335@gmail.com>
2024-12-17 17:21:09 +0100

    Comentarios
    He comentado el código.

--- src/Actividad_2/MatrizApp.java
+++ src/Actividad_2/MatrizApp.java
@@ -7,48 +7,48 @@ public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO: Implementar las funciones según las ramas asignadas
-    
+
 		// matriz-inicial
-		int[][] matriz = new int[5][5]; // Definir matriz.
+		int[][] numeros = new int[5][5]; // Definir matriz.
 
 		Random r = new Random(); // Añado el random que usaré para rellenar la matriz.
 
-		for (int i = 0; i < matriz.length; i++) { // Este bucle se encarga de las filas.
-			for (int j = 0; j < matriz.length; j++) { // Este bucle se encraga de las columnas.
-				matriz[i][j] = r.nextInt(101); // Relleno la matriz con el random y le establezco un rango de números
+		for (int i = 0; i < numeros.length; i++) { // Este bucle se encarga de las filas.
+			for (int j = 0; j < numeros.length; j++) { // Este bucle se encraga de las columnas.
+				numeros[i][j] = r.nextInt(101); // Relleno la matriz con el random y le establezco un rango de números
 												// que puede generar (1 - 100).
 			}
 		}
 
-		System.out.println("--- MATRIZ ---"); //Muestro un título para la matriz.
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				System.out.print(matriz[i][j] + " "); //Muestro la matriz con espacios.
+		System.out.println("--- MATRIZ ---"); // Muestro un título para la matriz.
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; j++) {
+				System.out.print(numeros[i][j] + " "); // Muestro la matriz con espacios.
 			}
-			System.out.println(); //Hago que se muestre línea por línea.
+			System.out.println(); // Hago que se muestre línea por línea.
 		}
 
 		// busqueda-numero
-		Scanner a = new Scanner(System.in);
-		
-		System.out.print("Busca un número: ");
-		int buscador = a.nextInt();
-		
-		boolean encontrado = false;
-		
-		for (int i = 0; i < numeros.length; i++) {
+		Scanner a = new Scanner(System.in); // Escáner para recoger los datos de los usuarios.
+
+		System.out.print("Busca un número: "); // Pregunto el número que se quiere buscar.
+		int buscador = a.nextInt(); // Guardo esos datos en la variable buscador.
+
+		boolean encontrado = false; // Boolean para determinar si el número se ha encontrado en la matriz.
+
+		for (int i = 0; i < numeros.length; i++) { //
 			for (int j = 0; j < numeros[i].length; j++) {
 
-				if (buscador == numeros[i][j]) {
-					encontrado = true;
-					System.out.println("El número esta en la fila " + i + ", columna " + j + ".");
-					break;
-				} 
+				if (buscador == numeros[i][j]) { // Mientras el número buscado sea igual a algun número de la matriz.
+					encontrado = true; // Cambiamos el boolean a true, porque hemos encontrado el número.
+					System.out.println("El número esta en la fila " + i + ", columna " + j + "."); // Muestro su posición.
+					break; // Salgo del if y del bucle.
+				}
 			}
 		}
-		
-		if (!encontrado) {
-			System.out.print("El número no se ha encontrado.");
+
+		if (!encontrado) { // Si no se ha encontrado el número.
+			System.out.print("El número no se ha encontrado."); // Muestro el mensaje.
 
 		}
 
